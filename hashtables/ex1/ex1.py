092985b4eb691f5233107443b36ba529d768fd2a
Kyle Pace <kpace2296@gmail.com>
2020-11-08 20:15:33 -0600

    finished ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
 
+    
+    # Your code here
+    hash_table = {}
+
+    if len(weights) <= 1:
+        return None
+
+    for x in weights:
+            hash_table[x] = x
+
+    for x in hash_table:
+        
+
+    print(hash_table)
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,39 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+    def __str__(self):
+        return f"{self.source} {self.destination}"
+
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+        
+    hash_table = {}
+    arr = []
+    count0 = 0
+    count1 = 1
+
+    for x in tickets:
+        split = str(x).split()
+        arr.append(split)
+
+    for x in range(0, length):
+        hash_table[arr[count0][0]] = arr[count0][1]
+        count0 += 1
+        count1 += 1
+    
+    route = []
+    route_num = 0
+
+    for x in range(0, length):
+        if len(route) < 1:
+            route.append(hash_table['NONE'])
+
+        else:
+            route.append(hash_table[route[route_num]])
+            route_num += 1
 
     return route
