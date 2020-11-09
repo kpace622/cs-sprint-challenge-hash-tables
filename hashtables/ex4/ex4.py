9bfb39ea70328678f77d9f4326a24aa018aefbb4
Kyle Pace <kpace2296@gmail.com>
2020-11-08 20:47:34 -0600

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result = []
+
+    for x in a:
+        hash_table[x] = x
+
+    for x in a:
+        if x < 0:
+            i = -x
+            print("i", i)
+            if hash_table.get(i) == None:
+                continue
+            else:
+                result.append(i)
 
     return result
 
